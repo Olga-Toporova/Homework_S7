43e353e5f90d145c8ccadd4293ce37f0c98888f0
Olga Toporova <sova795@yandex.ru>
2022-08-31 02:58:45 +0500

    Выполнены задания 47, 50, 52

---
+++ Ex_47/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+Console.Write("Введите количество строк (m): ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов (n): ");
+int column = int.Parse(Console.ReadLine());
+double[,] matrix = new double[row, column];
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        matrix[i, j] = new Random().Next(-10, 10);
+        Console.Write($"{matrix[i, j]}, ");
+    }
+    Console.WriteLine();
+}
+
---
+++ Ex_50/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+int[,] matrix = { {1, 4, 7, 2},
+                  {5, 9, 2, 3},
+                  {8, 4, 2, 4} };
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите значение строки: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите значение столбца: ");
+int column = int.Parse(Console.ReadLine());
+if (row > 0 && row < 4 && column > 0 && column < 5)
+{
+    Console.Write($"На указанной позиции находится число {matrix[row - 1, column - 1]}");
+}
+else Console.Write("Такого элемента нет");
